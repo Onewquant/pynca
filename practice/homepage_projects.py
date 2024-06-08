d869412dfd0909657c102f65a61d8640c813b0dc
onew <seunghwan0216@gmail.com>
2024-06-09 02:17:52 +0900

    homepage

---
+++ practice/homepage_projects.py
@@ -0,0 +1,65 @@
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+import pandas as pd
+import seaborn as sns
+import matplotlib.font_manager as fm
+
+# Seaborn 팔레트 생성
+palette = sns.color_palette('Blues_d')
+# palette = ListedColormap(["#87CEEB", "#4682B4", "#4169E1", "#0000CD","#00008B"])
+
+# Function to create pie charts for each sheet
+def create_pie_charts(excel_data, output_dir='/mnt/data/'):
+
+    # Pretendard 폰트 파일 경로 설정
+    font_path = 'C:/Windows/Fonts/Pretendard-Regular.ttf'  # Pretendard-Regular.ttf 파일 경로를 입력하세요
+    fontprop = fm.FontProperties(fname=font_path)
+
+
+    sheet_names = {'SIT':palette,'FIH':palette,'Field':palette}
+    # 'sequential5blau'
+    # 'sequential5petrol'
+    # sheet='Sheet3'
+    chart_paths = []
+    for sheet, color_p in sheet_names.items():
+        # Load data from the current sheet
+        df = pd.read_excel(excel_data, sheet_name=sheet)
+
+        # Assuming the first column is the category and the second column is the value
+        categories = df.iloc[:, 0]
+        values = df.iloc[:, 1]
+
+        # Create a pie chart
+        # plt.figure(figsize=(10, 10))
+
+        # 파이 차트 그리기
+        fig, ax = plt.subplots(figsize=(10, 10))
+        wedges, texts, autotexts = ax.pie(values, labels=categories, colors=palette, autopct='%1.1f%%', startangle=140)
+
+        # # Equal aspect ratio ensures that pie is drawn as a circle.
+        # ax.axis('equal')
+
+        # 파이 차트 안의 텍스트 폰트를 변경
+        for text in texts + autotexts:
+            text.set_fontproperties(fontprop)
+
+        # plt.pie(values, labels=categories, autopct='%1.1f%%', startangle=140, colors=nation4.colors)
+        # plt.pie(values, labels=categories, autopct='%1.1f%%', startangle=140, colors=palette)
+        # plt.title(f'Pie Chart for {sheet} Sheet')
+
+        # Save the pie chart as an image file
+        chart_path = f'{output_dir}/Projects_{sheet}.png'
+        plt.savefig(chart_path, bbox_inches='tight', dpi=300)
+        plt.close()
+
+        chart_paths.append(chart_path)
+
+    return chart_paths
+
+
+# Create pie charts for each sheet
+project_dir = 'C:/Users/ilma0/PycharmProjects/pynca/resource/piechart'
+excel_data=f'{project_dir}/Projects.xlsx'
+
+chart_paths = create_pie_charts(excel_data=excel_data, output_dir=project_dir)
+chart_paths
---
+++ practice/hompage_tdm.py
@@ -0,0 +1,74 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import matplotlib.font_manager as fm
+
+# Load the Excel file
+file_dir = 'C:/Users/ilma0/PycharmProjects/pynca/resource/piechart'
+file_path = f'{file_dir}/TDM.xlsx'
+chart_path = f'{file_dir}/TDM.png'
+excel_data = pd.ExcelFile(file_path)
+
+# Load the data from the first sheet
+df = pd.read_excel(excel_data, sheet_name=excel_data.sheet_names[0])
+
+# Set the style of the visualization
+sns.set(style="whitegrid")
+# font_path = 'C:/Users/ilma0/PycharmProjects/pynca/resource/piechart/Pretendard-1.3.9/public/variable/PretendardVariable.ttf' # 예시 경로
+# fontprop = fm.FontProperties(fname=font_path)
+# plt.rc('font', family=fontprop.get_name())
+
+# font_path = 'C:/Windows/Fonts/Pretendard-Regular.ttf'  # Pretendard-Regular.ttf 파일 경로를 입력하세요
+font_path = 'C:/Windows/Fonts/Pretendard-Bold.ttf'  # Pretendard-Regular.ttf 파일 경로를 입력하세요
+fm.fontManager.addfont(font_path)
+fontprop = fm.FontProperties(fname=font_path)
+# plt.rcParams['font.family'] = 'NanumGothic'
+#
+for font in fm.findSystemFonts(fontpaths=None, fontext='ttf'):
+   if 'Pretendard' in font:
+       print(font)
+#
+# # Pretendard 폰트 파일 경로 설정
+# font_path = 'C:/Windows/Fonts/Pretendard-Regular.ttf'  # Pretendard-Regular.ttf 파일 경로를 입력하세요
+#
+# # 폰트를 설정합니다.
+# fontprop = fm.FontProperties(fname=font_path)
+plt.rcParams['font.family'] = fontprop.get_name()
+
+# plt.rcParams['font.family'] = 'Pretendard'
+# font_list = [font.name for font in fm.fontManager.ttflist if font.name[0]=='P']
+# font_list
+# 팔레트 생성 및 역순 적용
+palette = sns.color_palette('Blues_d', n_colors=len(df['Year']))
+reversed_palette = list(reversed(palette))
+
+# Create a bar plot
+plt.figure(figsize=(20, 10))
+sns.barplot(x='Year', y='TDM', data=df, palette=reversed_palette, width=0.5)
+# sns.barplot(x='Year', y='TDM', data=df, palette='Blues_d', width=0.5)
+# sns.barplot(x='Year', y='TDM', data=df, palette='Blues', width=0.5)
+# sns.barplot(x='Year', y='TDM', data=df, color='deepskyblue', width=0.5)
+
+# Add title and labels
+
+
+
+plt.title('연도별 TDM 건수', fontsize=30, pad=30, loc='left', fontweight='bold')
+# plt.legend(title='', loc='upper right', labels=['SNUBH'], fontsize='large', bbox_to_anchor=(1, 1),
+#            frameon=False, framealpha=0.9, edgecolor='black', facecolor='lightgrey',
+#            title_fontsize='13', ncol=1, borderpad=1, labelspacing=0.5, handlelength=2,
+#            handletextpad=0.5)
+
+plt.tight_layout(pad=3.5)
+
+plt.xlabel('Year', fontsize=20, labelpad=8)
+plt.ylabel(None, fontsize=20, labelpad=8)
+
+plt.xticks(fontsize=18)
+plt.yticks(fontsize=18)
+plt.ylim(0, int(df['TDM'].max()*1.2/100)*100)
+
+# Display the plot
+plt.savefig(chart_path)
+plt.close()
+
